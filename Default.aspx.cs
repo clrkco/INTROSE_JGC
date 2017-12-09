786f1d0f09ad45722faad0b3b8462c1aa3e91a2e
clrkco <32839793+clrkco@users.noreply.github.com>
2017-12-09 23:05:05 +0800

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace INTROSE_JGC
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LoginView1_ViewChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
